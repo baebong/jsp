df978a8cc5f3f36a012f89b1d4ffa6afce037f45
94sylim <94sylim@gmail.com>
2021-12-27 15:38:21 +0900

    서블릿 생명주기(Servlet Life-Cycle) : 생성(init) >>> 실행(service) >>> 종료(destroy)

--- ServletRequestResponse/src/main/java/com/web/servlet/TestServletClass.java
+++ ServletRequestResponse/src/main/java/com/web/servlet/TestServletClass.java
@@ -1,6 +1,9 @@
 package com.web.servlet;
 
 import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServletClass extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException { // 실행
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("-- doGet() START 실행(service)단계 --");
 
 		// 사용자(User)가 어떤 요청을 하였는지 알고 싶을 때 request를 이용
 		// 요청은 사용자가 웹 어플리케이션에 무언가를 요청할 때 생기는 객체로 그 안에 사용자의 정보나 속성을 이용 가능
@@ -47,6 +51,27 @@ public class TestServletClass extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	// Servlet Life-Cycle : 생성(init) / 실행(service) / 종료(destroy)
+	@PostConstruct
+	public void funPC() { // 준비
+		System.out.println("-- @PostConstruct START 준비단계 --");
+	}
+
+	@Override
+	public void init() throws ServletException { // 생성
+		System.out.println("-- init() START 생성(init)단계 --");
+	}
+
+	@Override
+	public void destroy() { // 종료
+		System.out.println("-- destroy() START 종료(destroy)단계 --");
+	}
+
+	@PreDestroy
+	public void funPD() { // 소멸
+		System.out.println("-- @PreDestroy START 소멸단계 --");
 	}
 
 }
